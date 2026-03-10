4116331363e2a4b6b1c1894ceee3d4aeeffbb5e2
EstherRam <127980886+EstherRam@users.noreply.github.com>
2026-03-10 17:51:09 -0400

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import SentenceBuilding from "./pages/SentenceBuilding";
 import IAmFeeling from "./pages/IAmFeeling";
@@ -6,39 +6,44 @@ import IWant from "./pages/IWant";
 import GetHelp from "./pages/GetHelp";
 import EmergencyCall from "./pages/EmergencyCall";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Layout,
+      children: [
+        {
+          index: true,
+          Component: SentenceBuilding,
+        },
+        {
+          path: "i-am-feeling",
+          Component: IAmFeeling,
+        },
+        {
+          path: "i-want",
+          Component: IWant,
+        },
+        {
+          path: "get-help",
+          Component: GetHelp,
+        },
+        {
+          path: "help",
+          Component: GetHelp,
+        },
+      ],
+    },
+    {
+      path: "/call/:contact",
+      Component: EmergencyCall,
+    },
+    {
+      path: "/emergency-call",
+      Component: EmergencyCall,
+    },
+  ],
   {
-    path: "/",
-    Component: Layout,
-    children: [
-      {
-        index: true,
-        Component: SentenceBuilding,
-      },
-      {
-        path: "i-am-feeling",
-        Component: IAmFeeling,
-      },
-      {
-        path: "i-want",
-        Component: IWant,
-      },
-      {
-        path: "get-help",
-        Component: GetHelp,
-      },
-      {
-        path: "help",
-        Component: GetHelp,
-      },
-    ],
-  },
-  {
-    path: "/call/:contact",
-    Component: EmergencyCall,
-  },
-  {
-    path: "/emergency-call",
-    Component: EmergencyCall,
-  },
-]);
+    basename: "/Openvoiceaac/",
+  }
+);
