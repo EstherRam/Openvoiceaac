18f5c30a6fa0c36f1c1bdbaa84699c256064f56e
EstherRam <127980886+EstherRam@users.noreply.github.com>
2026-03-10 19:58:11 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
 
 export default defineConfig({
-  base: "/Openvoiceaac/",
+  base: "./",
 
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss()
   ],
 
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      "@": path.resolve(__dirname, "./src")
+    }
   },
 
-  assetsInclude: ['**/*.svg', '**/*.csv'],
+  assetsInclude: ["**/*.svg", "**/*.csv"]
 })
