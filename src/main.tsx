b18f2869f53e9617c78cf9267d652084d63f19cd
EstherRam <127980886+EstherRam@users.noreply.github.com>
2026-03-11 02:36:14 -0400

    Update import path for ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes";
-import { ThemeProvider } from "./contexts/ThemeContext";
+import { ThemeProvider } from "./app/contexts/ThemeContext";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
