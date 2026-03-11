56600efabe7426da9f1123241ef1657ce9802ae5
EstherRam <127980886+EstherRam@users.noreply.github.com>
2026-03-11 10:14:30 -0400

    Update import statement for react-router

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router';
+import { Link, useLocation } from "react-router-dom";
 import { useTheme } from '../contexts/ThemeContext';
 import {
   HelpCircle,
