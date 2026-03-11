ff32141e1f609b927b4c5088979cd55f6e647996
EstherRam <127980886+EstherRam@users.noreply.github.com>
2026-03-11 10:20:09 -0400

    Wrap RouterProvider with SpeechProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes";
+
 import { ThemeProvider } from "./app/contexts/ThemeContext";
+import { SpeechProvider } from "./app/contexts/SpeechContext";
+
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
-    <RouterProvider router={router} />
+    <SpeechProvider>
+      <RouterProvider router={router} />
+    </SpeechProvider>
   </ThemeProvider>
 );
