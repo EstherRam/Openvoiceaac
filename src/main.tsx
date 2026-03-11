d27f3c253abb7aa855939560884a46e7bf5c3e7f
EstherRam <127980886+EstherRam@users.noreply.github.com>
2026-03-11 02:21:31 -0400

    Wrap RouterProvider with ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes";
+import { ThemeProvider } from "./contexts/ThemeContext";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <ThemeProvider>
+    <RouterProvider router={router} />
+  </ThemeProvider>
 );
