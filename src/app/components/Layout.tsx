a63a3737bb097519d7108ab845e7c9b7823ae268
EstherRam <127980886+EstherRam@users.noreply.github.com>
2026-03-11 10:11:15 -0400

    Fix import statement for react-router-dom

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router";
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from "./Header";
 import SpeechBar from "./SpeechBar";
 import { ThemeProvider, useTheme } from "../contexts/ThemeContext";
